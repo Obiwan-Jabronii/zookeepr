1e39b728ea4c843123059dd2e05607d09c479ce1
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-07 15:28:53 -0400

    added PORT to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { animals } = require('./data/animals.json')
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -49,7 +50,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now listening on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now listening on port ${PORT}!`);
 });
 
